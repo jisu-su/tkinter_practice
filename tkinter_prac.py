4b4a87c2139548dbe3a1184c6994d271a53fbefb
박지수 <trleresoo@gmail.com>
2025-12-29 17:41:36 +0900

    조금 다른 방식으로 수정

--- tkinter_prac.py
+++ tkinter_prac.py
@@ -2,11 +2,10 @@ import tkinter as tk
 # 기본 창 생성
 root = tk.Tk()
 # 창 크기 설정
-root.geometry("400x300")
-# 창 배경색 설정
-root.configure(bg="black")
+root.geometry("500x300")
 # "Hello, Wordl!" 라벨 생성
-Label = tk.Label(root, text="Hello, World!", fg="white", bg="black", font=("Arial", 24))
-Label.pack(pady=10)
+Label = tk.Label(root, text="Hello, World!", fg="red", bg="black", font=("Courier", 24))
+# 좌우, 상하 여백 설정
+Label.pack(pady=20, padx=20)
 
 root.mainloop()
