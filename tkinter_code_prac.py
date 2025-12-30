d1052b5a3f144a45ed4551e9125caf0d9fe744c3
박지수 <trleresoo@gmail.com>
2025-12-30 12:49:06 +0900

    메서드화 도전.. 어렵다

--- tkinter_code_prac.py
+++ tkinter_code_prac.py
@@ -4,20 +4,22 @@ root = tk.Tk()
 # ? 왜 매개변수자리에 = 할당연산자가 있는거지?
 
 
-
 class Test_Data:
     def __init__(self, label_text, pady_value):
         self.label_text = label_text
         self.pady_value = pady_value
 
+    def create_label(self, text, pady):
+        self.text = text
+        self.pady = pady
+
+label = tk.Label(root, text=text)
+label.pack(pady=pady)
+
 test_date = Test_Data("Hello, Tkinter!", 10)
 
 label = tk.Label(root)
 
-
-def create_label(text, pady):
-    label = tk.Label(root, text=text)
-    label.pack(pady=pady)
 
 for _ in range(10):
     create_label(test_data["label_text"], test_data["pady_value"])
