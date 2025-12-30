634bf227c016d5a2f83e7ef93d1af5495b47cb82
박지수 <trleresoo@gmail.com>
2025-12-30 09:36:19 +0900

    버튼 만들어 보기

--- tkinter_prac.py
+++ tkinter_prac.py
@@ -1,13 +1,20 @@
 import tkinter as tk
 # 기본 창 생성
 root = tk.Tk()
+# 창 이름 정하기
+root.title("button")
 # 창 크기 설정
 root.geometry("500x300")
 # 창 배경색 설정
-root.configure(bg="lightblue")
-# "Hello, Wordl!" 라벨 생성
-Label = tk.Label(root, text="Hello, World!", fg="red", bg="white", font=("Courier", 35))
-# 정중앙에 만들어 보기
-Label.pack(expand=True)
+root.configure(bg="lightyellow")
+# 버튼을 누르면 실행될 함수 
+def click_button():
+    label.config(text="버튼이 클릭되었습니다")
+
+label= tk.Label(root, text="버튼을 눌러보세요", font=("맑은고딕",16))
+label.pack(pady=20)
+
+button = tk.Button(root, text="클릭", command=click_button)
+button.pack()
 
 root.mainloop()
