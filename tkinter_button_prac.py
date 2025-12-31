0b89a9e7a50afdd3b44a1b5cde7d741c5c0f29ac
박지수 <trleresoo@gmail.com>
2025-12-31 17:24:30 +0900

    최대한 코드 바꿔보기 끝!

--- tkinter_button_prac.py
+++ tkinter_button_prac.py
@@ -5,13 +5,6 @@ root = tk.Tk()
 # if문을 넣으면 좋을 거 같다. 만약에(if) 버튼이 눌린다면 “버튼이 클릭되었습니다”가 되도록 아니라면(else) 변하지 않는다. 
 # 창 크기랑 배경색도 묶을 수 있을까,,,?
 
-# 창 이름 정하기
-# root.title("button")
-# # 창 크기 설정
-# root.geometry("500x300")
-# # 창 배경색 설정
-# root.configure(bg="lightyellow")
-
 # 하드코딩 피하기! 변수를 한대 묶어 보기
 WINDOW_NAME = "button"
 WINDOW_SIZE = "500x300"
@@ -24,10 +17,6 @@ def set_window():
 # 함수 실행을 위해 호출해주기
 set_window()
 
-# 버튼을 누르면 실행될 함수 
-# def click_button():
-#     label.config(text="버튼이 클릭되었습니다")
-
 #함수 안에 if문 넣기
 # def click_button():
 #     # cget을 이용해서 버튼을 누를 때마다 다른 문구가 나오도록 설정(토글)
@@ -35,7 +24,7 @@ set_window()
 #         label.config(text="버튼이 클릭되었습니다")
 #     else :
 #         label.config(text="버튼을 눌러보세요")
-# 삼항 연산자 이용하기 (얘도 함수가 필요하다)
+# 삼항 연산자 이용하기 (얘도 함수가 필요하다) , 토글
 def click_button():
     new_text = "버튼이 클릭되었습니다" if label.cget("text") == "버튼을 눌러보세요" else "버튼을 눌러보세요"
     label.config(text=new_text)
